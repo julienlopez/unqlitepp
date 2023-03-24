32c776b98f0fdce9425d4f7b858c80c9c88befea
julien <julien.lopez51@gmail.com>
2023-03-24 19:44:38 +0100

    adding string append as well

--- unit_tests/test_basic.cpp
+++ unit_tests/test_basic.cpp
@@ -7,7 +7,6 @@ using namespace std::string_literals;
 TEST_CASE("Basic store and fetch workflow")
 {
     auto db = unqlitepp::openInMemory(unqlitepp::OpenMode::ReadWrite);
-    // auto db = unqlitepp::openLocalFile("./test.db", unqlitepp::OpenMode::Create);
     REQUIRE(db.has_value());
 
     const auto key1 = "key1";
@@ -41,4 +40,12 @@ TEST_CASE("Basic store and fetch workflow")
         REQUIRE(res_fetch2.has_value());
         CHECK(*res_fetch2 == data2);
     }
+
+    const auto data2_suffix = ", and some more"s;
+    const auto res_append = unqlitepp::append(*db, key2, data2_suffix);
+    REQUIRE(res_append.has_value());
+
+    const auto res_fetch = unqlitepp::fetch(*db, key2);
+    REQUIRE(res_fetch.has_value());
+    CHECK(*res_fetch == data2 + data2_suffix);
 }
